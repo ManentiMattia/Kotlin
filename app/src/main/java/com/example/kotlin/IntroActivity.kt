da7f207855a34ff21654c0b38ef48b3496300b1f
ManentiMattia <minemattia06@gmail.com>
2025-03-21 21:54:12 +0100

    Prima commit

---
+++ app/src/main/java/com/example/kotlin/IntroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
