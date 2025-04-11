3fdb5867082687877d7057190de52b2f650e3d69
BhyronLove6 <165470341+BhyronLove6@users.noreply.github.com>
2025-04-11 12:53:17 +0200

    Aggiunte
    -SliderAdapter
    -Problema nel MainActivity

--- app/src/main/java/com/example/kotlin/Adapter/SliderAdapter.kt
+++ app/src/main/java/com/example/kotlin/Adapter/SliderAdapter.kt
@@ -29,7 +29,7 @@ class SliderAdapter(
 
 
     class SliderViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        private val imageView:ImageView=itemView.findViewById(R.id.ImageSlide)
+        private val imageView:ImageView=itemView.findViewById(R.id.imageSlide)
 
         fun setImage(sliderItems:SliderModel ,context:Context){
             val requestOptions=RequestOptions().transform(CenterInside())
@@ -37,7 +37,7 @@ class SliderAdapter(
             Glide.with(context)
                 .load(sliderItems.url)
                 .apply(requestOptions)
-                .into(ImageView)
+                .into(imageView)
 
         }
     }
@@ -52,11 +52,8 @@ class SliderAdapter(
         return SliderViewHolder(view)
     }
 
-    override fun onBindViewHolder(
-        holder: SliderAdapter.SliderViewHolder,
-        position: Int
-    ) {
-        holder.setImage(sliderItems(position),context)
+    override fun onBindViewHolder(holder: SliderAdapter.SliderViewHolder, position: Int) {
+        holder.setImage(sliderItems[position], context)
 
         if(position==sliderItems.lastIndex-1){
             viewPager2.post(runnable)
--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.kotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.kotlin.Adapter.SliderAdapter
+import com.example.kotlin.Model.SliderModel
+import com.example.kotlin.databinding.ActivityMainBinding
 
-open class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel = MainViewModel()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) {
-            v, insets -> val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        initBanners()
+
     }
+
+    private fun initBanners(){
+        binding.progressBarBanner.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer{
+            banners(it)
+            binding.progressBarBanner.visibility=View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners (image: List<SliderModel>){
+        binding.viewPagerSlider.adapter=SliderAdapter(images, binding.viewPagerSlider)
+        binding.viewPagerSlider.clipToPadding=false
+        binding.viewPagerSlider.clipChildren=false
+        binding.viewPagerSlider.offscreenPageLimit=3
+        binding.viewPagerSlider.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
+
+        val compositoPageTransformer = CompositoPageTransformer().apply{
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewPagerSlider.setPageTransformer(compositoPageTransformer)
+        if(images.size>1){
+            binding.dotIndicator.visibility.View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewPagerSlider)
+        }
+
+    }
+
 }
